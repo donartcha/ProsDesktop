8c40c8efb98b97d238db2d05f3b32ecbc33f1da4
Dony <Dony@192.168.0.195>
2013-05-29 13:39:37 +0000

    PureFX Experimental for NiconDesktop

--- src/nicon/NiconDesktop/guifx/DesktopViewer/DesktopViewerController.java
+++ src/nicon/NiconDesktop/guifx/DesktopViewer/DesktopViewerController.java
@@ -4,13 +4,19 @@
  */
 package nicon.NiconDesktop.guifx.DesktopViewer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
 
 /**
  * FXML Controller class
@@ -18,8 +24,22 @@ import javafx.fxml.Initializable;
  * @author Dony
  */
 public class DesktopViewerController implements Initializable {
+private Parent navigator; 
+private AnchorPane AppContenedor;
 
+
+
+
+    @FXML
+private AnchorPane desktop;
+    
    
+   
+    @FXML
+    private MenuItem closeIN;
+    @FXML
+    private MenuItem openIN;
+    
     
     
     
@@ -36,6 +56,40 @@ public class DesktopViewerController implements Initializable {
     System.exit(0);
     }
     
+    @FXML
+    private void OpenNiconNavigator(){
+    
+        try {
+            navigator = FXMLLoader.load(nicon.NiconDesktop.guifx.NiconNavigator.NavegadorController.class.getResource("Navegador.fxml"));
+            navigator.setVisible(true);
+            
+            
+            AnchorPane.setTopAnchor(navigator, 0.0);
+            AnchorPane.setBottomAnchor(navigator, 0.0);
+            AnchorPane.setLeftAnchor(navigator, 0.0);
+            AnchorPane.setRightAnchor(navigator, 0.0);
+            AppContenedor = new AnchorPane();
+            AppContenedor.setStyle("-fx-background-color:linear-gradient(#000000, #FF7700);");
+            AppContenedor.getChildren().add(navigator);
+            AnchorPane.setTopAnchor(AppContenedor, 0.0);
+            AnchorPane.setBottomAnchor(AppContenedor, 0.0);
+            AnchorPane.setLeftAnchor(AppContenedor, 0.0);
+            AnchorPane.setRightAnchor(AppContenedor, 0.0);
+            desktop.getChildren().add(AppContenedor);
+            openIN.setVisible(false);
+            closeIN.setVisible(true);
+        } catch (IOException ex) {
+           System.out.println("Error OpenNavigator"+ex);
+        }
+    }
+    
+    @FXML
+    private void CloseNiconNavigator(){
+    desktop.getChildren().remove(AppContenedor);
+    openIN.setVisible(true);
+    closeIN.setVisible(false);
+    }
+    
     
     
      /**
@@ -43,6 +97,17 @@ public class DesktopViewerController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+    closeIN.setVisible(false); 
+    
+ 
+   
+    
+    
+  
+      
+   //Platform.runLater(timer);
+    
+
         // TODO
     }    
 }
---
+++ src/nicon/NiconDesktop/object/Icono.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nicon.NiconDesktop.object;
+
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author Dony
+ */
+public class Icono extends Pane{
+    public Icono(){
+this.setWidth(50);
+this.setHeight(50);
+this.setStyle("-fx-background-color: linear-gradient(#61a2b1, #2A5058);");
+}    
+}
